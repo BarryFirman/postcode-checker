85ae4de9b11deefccb0909eacc6e57861016ae52
Barry Firman <barryfirman@outlook.com>
2021-02-07 22:10:21 +0000

    Controller and Routing tests pass.

--- app/controllers/postcode_allow_lists_controller.rb
+++ app/controllers/postcode_allow_lists_controller.rb
@@ -1,3 +1,45 @@
-class PostcodeAllowListsController < ApplicationController
+# frozen_string_literal: true
 
+class PostcodeAllowListsController < ApplicationController
+  before_action :ensure_postcode_allow_list, only: %i[destroy]
+  before_action :postcode_allow_list, only: %i[destroy]
+
+  def index
+    @postcode_allow_lists = PostcodeAllowList.all
+  end
+
+  def new
+    @postcode_allow_list = PostcodeAllowList.new
+  end
+
+  def create
+    postcode_allow_list = PostcodeAllowList.new(postcode_allow_lists_params)
+    if postcode_allow_list.save
+      redirect_to postcode_allow_lists_path, notice: 'Post Code successfully created.'
+    else
+      render :new, notice: 'Could not create Post Code.'
+    end
+  end
+
+  def destroy
+    @postcode_allow_list.destroy
+    redirect_to postcode_allow_lists_url
+  end
+
+
+  private
+
+  def postcode_allow_lists_params
+    params.require(:postcode_allow_lists).permit(:id, :postcode)
+  end
+
+  def postcode_allow_list
+    @postcode_allow_list = PostcodeAllowList.find params[:id]
+  end
+
+  def ensure_postcode_allow_list
+    return if PostcodeAllowList.find_by id: params[:id]
+
+    redirect_to postcode_allow_lists_url, notice: 'Could not find postcode entry'
+  end
 end
--- spec/requests/postcode_allow_lists_request_spec.rb
+++ spec/requests/postcode_allow_lists_request_spec.rb
@@ -22,25 +22,58 @@ RSpec.describe "PostcodeAllowLists", type: :request do
     context 'with valid parameters' do
       it 'creates new postcode' do
         expect do
-          post postcode_allow_lists_url, params: { postcode_allow_list: valid_attributes }
+          post postcode_allow_lists_url, params: { postcode_allow_lists: valid_attributes }
         end.to change(PostcodeAllowList, :count).by(1)
       end
 
       it 'redirects to #index' do
-        post postcode_allow_lists_url, params: { postcode_allow_list: valid_attributes }
+        post postcode_allow_lists_url, params: { postcode_allow_lists: valid_attributes }
         expect(response).to redirect_to(postcode_allow_lists_url)
       end
+    end
+
+    describe 'DELETE /destroy' do
+      context 'with valid data' do
+        it 'destroys the requested PostcodeAllowList' do
+          postcode_allow_list = PostcodeAllowList.create! valid_attributes
+          expect do
+            delete postcode_allow_list_url(postcode_allow_list)
+          end.to change(PostcodeAllowList, :count).by(-1)
+        end
+
+        it 'redirects to #index' do
+          postcode_allow_list = PostcodeAllowList.create! valid_attributes
+          delete postcode_allow_list_url(postcode_allow_list)
+          expect(response).to redirect_to(postcode_allow_lists_url)
+        end
+      end
+
+      # context 'with invalid data' do
+      #   it 'does not destroy a PostcodeAllowList record' do
+      #     postcode_allow_list = PostcodeAllowList.create!(valid_attributes)
+      #     expect do
+      #       delete postcode_allow_list_url, params: { postcode_allow_lists: { id: postcode_allow_list.id + 1 } }
+      #     end.to change(PostcodeAllowList, :count).by(0)
+      #   end
+      #
+      #   it 'redirects to #index' do
+      #     postcode_allow_list = PostcodeAllowList.create!(valid_attributes)
+      #     delete postcode_allow_list_url, params: { postcode_allow_lists: { id: postcode_allow_list.id + 1 } }
+      #     expect(response).to redirect_to(postcode_allow_lists_url)
+      #   end
+      # end
+
     end
 
     context 'with invalid parameters' do
       it 'does not create a new postcode' do
         expect do
-          post postcode_allow_lists_url, params: { postcode_allow_list: invalid_attributes }
+          post postcode_allow_lists_url, params: { postcode_allow_lists: invalid_attributes }
         end.to change(PostcodeAllowList, :count).by(0)
       end
 
       it 'is successful' do
-        post postcode_allow_lists_url, params: { postcode_allow_list: invalid_attributes }
+        post postcode_allow_lists_url, params: { postcode_allow_lists: invalid_attributes }
         expect(response).to be_successful
       end
     end
