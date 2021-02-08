fcb836bdceb220331cfc16f017ace4b99578fd88
Barry Firman <barryfirman@outlook.com>
2021-02-08 15:38:24 +0000

    Created Home page route to pass tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'postcode_checker/check'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root 'pages#home'
   # postcode_allow_lists
   resources :postcode_allow_lists, only: [:index, :new, :create, :destroy]
+
+
 end
