3aea5d6e539ba4a06f2b3a40ebe4d5695b44de92
Barry Firman <barryfirman@outlook.com>
2021-02-12 08:47:52 +0000

    Created postcode_checker controller and updated tests.

--- app/controllers/postcode_checker_controller.rb
+++ app/controllers/postcode_checker_controller.rb
@@ -1,5 +1,34 @@
 class PostcodeCheckerController < ApplicationController
-  def check
+  before_action :ensure_params_present
+  before_action :ensure_postcode
 
+  def check
+    checker = Services::PostcodeChecker.new(postcode: postcode_checker_params[:postcode])
+    @response = checker.check_postcode
+    @notice = if @response[:status] == 'success'
+                "We can#{'not' unless @response[:in_area]} service area:  #{postcode_checker_params[:postcode]}"
+              else
+                @response[:message]
+              end
+    render :check, notice: @notice
   end
+
+  private
+
+  def postcode_checker_params
+    params.require(:postcode_checker).permit(:postcode)
+  end
+
+  def ensure_params_present
+    return if params[:postcode_checker].present?
+
+    render :check, notice: 'Error: Please ensure you have entered a postcode.'
+  end
+
+  def ensure_postcode
+    return if params[:postcode_checker][:postcode].present?
+
+    render :check, notice: 'Error: Please ensure you have entered a postcode.'
+  end
+
 end
--- spec/requests/postcode_checker_request_spec.rb
+++ spec/requests/postcode_checker_request_spec.rb
@@ -8,26 +8,53 @@ RSpec.describe 'PostcodeChecker', type: :request do
   let(:postcode_allow_list) { create(:postcode_allow_list, :valid) }
 
   describe 'POST /check' do
-    it 'is a successful response for check' do
+    it 'should return a successful response for #check' do
       post check_postcode_url, params: { postcode_checker: in_area_postcode }
       expect(response).to be_successful
     end
+
+    it 'should return successful with invalid params' do
+      post check_postcode_url params: in_area_postcode
+      expect(response).to be_successful
+    end
+
   end
 
   describe 'responses' do
     context 'valid data' do
-      it 'should return success for allowed postcode.' do
-        post check_postcode_url, params: { postcode_checker: allowed_postcode }
+      it 'should return success for allowed postcode' do
+        postcode_allow_list.save!
+        post check_postcode_url, params: { postcode_checker: { postcode: postcode_allow_list.postcode } }
         expect(response).to be_successful
       end
 
-      it 'should return success for out of area postcode'do
+      it 'should return successful response for out of area postcode' do
         post check_postcode_url, params: { postcode_checker: out_area_postcode }
         expect(response).to be_successful
       end
     end
 
     context 'invalid data' do
+      it 'should return a successful response for blank param' do
+        post check_postcode_url, params: { postcode_checker: '' }
+        expect(response).to be_successful
+      end
+
+      it 'should return a successful response for nil params' do
+        post check_postcode_url, params: { postcode_checker: nil }
+        expect(response).to be_successful
+      end
+
+      it 'should return a successful response for blank postcode' do
+        post check_postcode_url, params: { postcode_checker: { postcode: '' } }
+        expect(response).to be_successful
+      end
+
+      it 'should return a successful response for nil postcode' do
+        post check_postcode_url, params: { postcode_checker: { postcode: nil } }
+        expect(response).to be_successful
+      end
+
       it 'should return success for invalid postcode' do
         post check_postcode_url, params: { postcode_checker: invalid_postcode }
         expect(response).to be_successful
