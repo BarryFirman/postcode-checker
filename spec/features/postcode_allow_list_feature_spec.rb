d8b07a5556737299d870af7d0a31aba963aab672
Barry Firman <barryfirman@outlook.com>
2021-02-13 07:06:59 +0000

    WIP: Started framework for tests.

---
+++ spec/features/postcode_allow_list_feature_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+Rspec.describe PostcodeAllowList, type: :feature do
+  context 'Valid data' do
+    scenario 'User adds valid postcode to list' do
+
+    end
+
+    scenario 'User edits postcode in list' do
+
+    end
+
+    scenario 'User deletes postcode in list' do
+
+    end
+  end
+
+  context 'Invalid data' do
+    scenario 'User attempts to add invalid data to list' do
+    end
+  end
+
+end
